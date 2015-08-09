b506e442e123dcdc14e96911e7f517fe032dc52a
unknown <lenlaurito@gmail.com>
2015-08-09 18:27:34 +0800

    'update'

--- src/main/java/hello/Greeting.java
+++ src/main/java/hello/Greeting.java
@@ -2,12 +2,16 @@ package hello;
 
 public class Greeting {
 
-    private final long id;
-    private final String content;
+    private long id;
+    private String content;
 
     public Greeting(long id, String content) {
         this.id = id;
         this.content = content;
+    }
+
+    public Greeting() {
+
     }
 
     public long getId() {
