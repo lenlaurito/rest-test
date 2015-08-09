2e4b499a6bc6d0e7c58edfa47d55fad23dd378d6
unknown <lenlaurito@gmail.com>
2015-08-09 19:02:02 +0800

    'updateagain'

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -3,6 +3,8 @@ package hello;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.apache.commons.logging.Log;
 
@@ -44,8 +46,11 @@ public class GreetingController {
     }
 
     @RequestMapping(value= "/greeting/{id1}", method = RequestMethod.GET)
-    public Greeting getGreeting(@PathVariable("id1") long id) {
+    public ResponseEntity<Greeting> getGreeting(@PathVariable("id1") long id) {
         Greeting greeting = greetingMap.get(id);
-        return greeting;
+        if(greeting == null) {
+            return new ResponseEntity<Greeting>(HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<Greeting>(greeting, HttpStatus.OK);
     }
 }
